4eb5f4f3f3ccfa9f8dac2bce3a0f00f6c39747f0
RobotNinja07 <57858475+RobotNinja07@users.noreply.github.com>
2022-05-11 16:42:22 -0700

    dsadaszzz

--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::get('/callback', function (Request $request) {   //Get Token after authorization
-    $state = $request->session()->pull('state');
 
-    if(strlen($state) > 0 && $state === $request->state) {
-
-        $response = Http::asForm()->post('https://cse4500-practice.herokuapp.com/oauth/token', [
-            'grant_type' => 'authorization_code',
-            'client_id' => '3',
-            'client_secret' => 'FtKOl9aXE8GVabVvkkACmwJo2XxfX1l5v1vGSNE1',
-            'redirect_uri' => 'https://tinyurl-api.herokuapp.com/callback',
-            'code' => $request->code,
-        ]);
-
-        $accessToken = $response->json()['access_token'];
-
-        //Use the token to request data
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => 'Bearer '.$accessToken,
-        ])->get('https://cse4500-practice.herokuapp.com/api/users');
-
-        return $response->json();
-
-    } else {
-        return redirect()->route('authorization');
-    }
-});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -64,3 +38,31 @@ Route::get('/authorization', function (Request $request) {  //Get authorization
 
     return redirect('https://cse4500-practice.herokuapp.com/oauth/authorize?'.$query);
 })->name('authorization');
+
+Route::get('/callback', function (Request $request) {   //Get Token after authorization
+    $state = $request->session()->pull('state');
+
+    if(strlen($state) > 0 && $state === $request->state) {
+
+        $response = Http::asForm()->post('https://cse4500-practice.herokuapp.com/oauth/token', [
+            'grant_type' => 'authorization_code',
+            'client_id' => '3',
+            'client_secret' => 'FtKOl9aXE8GVabVvkkACmwJo2XxfX1l5v1vGSNE1',
+            'redirect_uri' => 'https://cse4500-practice.herokuapp.com/callback',
+            'code' => $request->code,
+        ]);
+
+        $accessToken = $response->json()['access_token'];
+
+        //Use the token to request data
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$accessToken,
+        ])->get('https://cse4500-practice.herokuapp.com/api/users');
+
+        return $response->json();
+
+    } else {
+        return redirect()->route('authorization');
+    }
+});
